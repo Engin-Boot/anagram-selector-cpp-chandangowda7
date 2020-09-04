bcc3abee28c5021c55a3e01d78c6e8a4e33f8e83
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-04 17:08:06 +0530

    Update anagram.cpp

--- anagram.cpp
+++ anagram.cpp
@@ -2,7 +2,18 @@
 
 bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
     //Fill the correct implementation here
-    return false;
+    int lenofword1=word1.length();
+    int lenofword2=word2.length();
+    if(lenofword1!=lenofword2)
+        return false;
+    sort(word1.begin(),word1.end());
+    sort(word2.begin(),word2.end());
+    for(int i=0;i<lenofword1;i++)
+    {if(word1[i]!=word2[i])
+        return false;
+    }
+    return true;
+
 }
 
 std::vector<std::string> Anagram::SelectAnagrams(
