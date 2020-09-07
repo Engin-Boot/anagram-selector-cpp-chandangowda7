824b385b325ebb668787a6be569ceaee9ef04ec3
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-07 12:18:31 +0530

    Update anagram.h

--- anagram.h
+++ anagram.h
@@ -4,7 +4,7 @@
 namespace Anagram
 {
     bool WordPairIsAnagram(const std::string& word1, const std::string& word2);
-
+int* allocateToarray(const std::string& word);
     std::vector<std::string> SelectAnagrams(
         const std::string& word,
         const std::vector<std::string>& candidates);
